bb1d5e34bb5c3876fe5cedaa5677b99f7e1d81cd
József Samu <sjozsef@fps.hu>
2017-10-06 17:29:29 +0200

    First commit

---
+++ src/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <p>Counter: {{counter}}</p>
+        <button @click="increment">Click me!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Hello Vue',
+            counter: 0
+        }
+    }, 
+    methods: {
+        increment( evt ) {
+            this.counter++;
+        }
+    }
+}
+</script>
